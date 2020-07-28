ac45f3a6c911033223cfc080c7dbbc26dad2242e
sayantan-bhattacharje <s.bhattacharjee00@gmail.com>
2020-07-29 01:58:03 +0530

    second commit

---
+++ App.js
@@ -0,0 +1,20 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React, {Component} from 'react';    
+import TabScreen from './src/Screens/TabScreen'; 
+  
+export default class App extends Component {  
+  render() {  
+    return (  
+      <TabScreen/> 
+    );  
+  }  
+}  
+
+
---
+++ src/Screens/TabScreen.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import { Container, Header, Tab, Tabs, Left, Body, Right, Title, } from 'native-base';
+import Tab1 from './Tabs/tab1';
+import Tab2 from './Tabs/tab2';
+import Tab3 from './Tabs/tab3';
+export default class TabsExample extends Component {
+  render() {
+    return (
+      <Container>
+        <Header hasTabs >
+          
+           
+              <Title>News App</Title>
+            
+          
+        </Header>
+        <Tabs>
+          <Tab heading="Tab1">
+            <Tab1 />
+          </Tab>
+          <Tab heading="Tab2">
+            <Tab2 />
+          </Tab>
+          <Tab heading="Govt">
+            <Tab3 />
+          </Tab>
+        </Tabs>
+      </Container>
+    );
+  }
+}
+
